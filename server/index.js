d896de974d8bf1d40d83aa5ceaa543385fba4e1c
Matthew Beckwith <mbeckwith@morganautogroup.com>
2026-04-21 10:13:16 -0400

    Server calls adjusted and working

--- server/index.js
+++ server/index.js
@@ -4,6 +4,23 @@ const cors = require("cors");
 const app = express();
 const PORT = 8080;
 
+const inventoryBoardId = 18400257085;
+const inventoryBoardColumns = {
+  uid: 'pulse_id_mm22jxg8',
+  status: 'status',
+  store: 'multi_selecti0j9cnxp',
+  carType: 'single_selectdn7oazf',
+  titleOrPayoff: 'single_select1h3ilkm',
+  titleStatus: 'single_selecteuuhmw9',
+  payoffAmount: 'numberi3tr3ou7',
+  goodTillDate: 'datexdlnpy7n',
+  lienHolder: 'short_textx0819yu8',
+  perDiam: 'number9da8ds1t',
+  notes: 'long_texthgdlm5pj',
+  attachments: 'file30p9d1mi',
+  priority: 'color_mm1rnnyt'
+};
+
 app.use(cors());
 
 const apiMiddleware = (req, res, next) => {
@@ -35,35 +52,67 @@ app.get('/auth', (req, res) => {
 app.get('/oauth/callback', async (req, res) => {
   const { code } = req.query;
 
-  const response = await fetch('https://auth.monday.com/oauth2/token', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      redirect_uri: process.env.REDIRECT_URI,
-      code: code
-    })
-  });
-
-  const data = await response.json();
-  const accessToken = data.access_token;
-  res.send(`Access Token: ${accessToken}`)
+  try {
+      const response = await fetch('https://auth.monday.com/oauth2/token', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          client_id: process.env.CLIENT_ID,
+          client_secret: process.env.CLIENT_SECRET,
+          redirect_uri: process.env.REDIRECT_URI,
+          code: code
+        })
+      });
+    
+      const data = await response.json();
+      const accessToken = data.access_token;
+      console.log("Token", accessToken);
+      res.redirect("https://bad76-service-32093452-c0a7fb28.us.monday.app")
+  } catch (err) {
+    console.log("OAUTH ERROR", err);
+  }
 });
 
 app.get('/', (req, res) => {
-    res.send("Hola...");
+    res.send("Monday Integration");
 });
 
 app.get('/test', apiMiddleware, async (req, res) => {
 try {
     const apiResult = await req.fetchToApi({
-        'query': 'query { boards (ids: 18400257085) { name } }'
+        'query': `query { boards (ids: ${inventoryBoardId}) { name } }`
     });
     res.status(200).json(apiResult);
   } catch (error) {
     res.status(500).send('API Error');
   }
+});
+
+app.get('/get-items', apiMiddleware, async (req, res) => {
+    const columnIds = Object.values(inventoryBoardColumns).map(id => `"${id.toString()}"`).join(',');
+    try {
+        const apiResult = await req.fetchToApi({
+            'query': `query { 
+                boards (ids: ${inventoryBoardId}) {
+                    items_page(limit: 500) {
+                        cursor
+                        items {
+                            id
+                            name
+                            column_values(ids: [${columnIds}]) {
+                                id
+                                text
+                                value
+                            }
+                        }
+                    }
+                }
+            }`
+        });
+        res.status(200).json(apiResult);
+    } catch (error) {
+        res.status(500).send('API Error');
+    }
 });
 
 app.listen(PORT, () => {
